87565b52395498f16b1180920f793fcbb29325ab
kangwubin <2624656980@qq.com>
2019-12-17 00:04:04 +0800

    1.使用jconsole命令查看系统当前的进程 2.线程当中的方法的使用 3.线程状态的转换 4.synchronized锁的应用 5.synchronized、volatile确保线程安全的方式 6.单例模式代码的深入理解 7.wait()\notify()的使用与理解

--- Thread/src/xatu20191216/ArrayQueue.java
+++ Thread/src/xatu20191216/ArrayQueue.java
@@ -1,7 +1,9 @@
 package xatu20191216;
 
+import java.util.Random;
+
 /**
- * Description:
+ * Description:循环队列
  *
  * @author: KangWuBin
  * @Date: 2019/12/16
@@ -13,5 +15,78 @@ public class ArrayQueue {
     private int front = 0;  //队头
     private int rear = 0;  //队尾
 
+    public void put(int val) {
+        if (size == array.length) {
+            throw new RuntimeException("满了");
+        }
+        array[rear] = val;
+        rear = (rear + 1) % array.length;
+        /*array[rear] = val;
+        rear++;
+        if (rear == array.length) {
+            rear = 0;
+        }*/
+        size++;
+    }
 
+    public int take() {
+        if (size == 0) {
+            throw new RuntimeException("空了");
+        }
+        int val = array[front];
+        front = (front + 1) % array.length;
+        size--;
+        return val;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    private static ArrayQueue queue = new ArrayQueue();
+
+    private static class Producer extends Thread {
+        Producer() {
+            super("生产者：");
+        }
+
+        @Override
+        public void run() {
+            Random random = new Random(20191216);
+            while (true) {
+                int val = random.nextInt();
+                queue.put(val);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static class Customer extends Thread {
+        Customer() {
+            super("消费者：");
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                int val = queue.take();
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Producer producer = new Producer();
+        producer.start();
+        Customer customer = new Customer();
+        customer.start();
+    }
 }
